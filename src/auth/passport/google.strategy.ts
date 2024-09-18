d0ba2850173ff041cdafbf29269e0968a5fac467
quangtin28bh <lequangtin_t67@hus.edu.vn>
2024-09-18 10:11:10 +0700

    update google and jwt strategy

---
+++ src/auth/passport/google.strategy.ts
@@ -0,0 +1,34 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(private configService: ConfigService) {
+    super({
+      clientID:
+        '1094263552219-1fc8fj4a3o2q5falk7252pcr755ha55o.apps.googleusercontent.com',
+      clientSecret: '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',
+      callbackURL: 'http://localhost:4000/login/google/redirect',
+      scope: ['email', 'profile'],
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: Profile,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+    };
+    done(null, user);
+  }
+}
---
+++ src/auth/passport/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IUser } from '../../users/users.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: IUser) {
+    const { name, _id, email } = payload;
+    return {
+      message: 'Get user success',
+      name: name,
+      email: email,
+      id: _id,
+    };
+  }
+}
